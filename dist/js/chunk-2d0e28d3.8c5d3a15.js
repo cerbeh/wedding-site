6aa0b21672a15a7966b1fe312db9b11ffa53d4b7
Cerbeh <cerbeh@gmail.com>
2019-10-20 14:07:26 +0100

    Initial dist subtree commit

---
+++ dist/js/chunk-2d0e28d3.8c5d3a15.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e28d3"],{"7ed3":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("h1",[e._v("Ceremony")])},c=[],r={},u=r,o=t("2877"),s=Object(o["a"])(u,a,c,!1,null,"12aa6d96",null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0e28d3.8c5d3a15.js.map
