110148be2af84b0b022d04c8914e532b6eccb3d6
Cerbeh <cerbeh@gmail.com>
2019-10-26 11:40:52 +0100

    Added pages

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -1,17 +1,39 @@
 <template lang="html">
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/charities">Charities</router-link> |
-    <router-link to="/ceremony">Ceremony</router-link> |
-    <router-link to="/party">Party</router-link>
+  <div class="">
+      <a-tabs defaultActiveKey="1">
+
+        <a-tab-pane tab="Tab 1" key="1">
+          <router-link to="/">Home</router-link>
+        </a-tab-pane>
+
+        <a-tab-pane tab="Tab 2" key="2" forceRender>
+          <router-link to="/ceremony">Ceremony</router-link>
+        </a-tab-pane>
+
+        <a-tab-pane tab="Tab 3" key="3">
+          <router-link to="/party">Party</router-link>
+        </a-tab-pane>
+
+        <a-tab-pane tab="Tab 4" key="4">
+          <router-link to="/charities">Charities</router-link>
+        </a-tab-pane>
+
+      </a-tabs>
   </div>
 </template>
 
 <script>
+
+// const { TabPane, TabContent } = Tabs
+
+
 export default {
-  name: 'NavBar'
+  name: 'NavBar',
+  // components: { Tabs.name, Tabs },
+  methods: {
+    // callback(key) {
+    //   console.log(key)
+    // }
+  }
 }
 </script>
-
-<style lang="css" scoped>
-</style>
