2787b5c009353dbb10270e8c8d109175ea453579
Cerbeh <cerbeh@gmail.com>
2019-12-09 10:06:50 +0000

    Removed ant-design and added buefy and vue breakpoints

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Buefy from 'buefy';
+import { Plugin as VBreakpoint } from 'vue-breakpoint-component'
+
+import 'buefy/dist/buefy.css';
 
 Vue.config.productionTip = false
+
+Vue.use(Buefy);
+Vue.use(VBreakpoint)
 
 new Vue({
   router,
