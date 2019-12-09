ac216b3ec81995a6ddd2aab9ef8bf27e8cff2050
Cerbeh <cerbeh@gmail.com>
2019-12-09 10:10:53 +0000

    Added placeholder copy for homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template lang="html">
-  <h1>Home</h1>
+  <div class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="content">
+        <h2>Invitation thing</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+        <p>28.02.2020</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
