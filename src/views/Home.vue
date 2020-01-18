cac9a3b1ed389c9c3bba40f5026f123b3bf741e1
Cerbeh <cerbeh@gmail.com>
2020-01-18 16:47:36 +0000

    Added styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div class="hero is-fullheight">
+  <div class="hero is-fullheight-with-navbar">
     <div class="hero-body">
       <figure class="image hero-footer is-5by5">
         <img src="@/assets/happy_couple2.jpg" class="is-inline"/>
@@ -7,7 +7,7 @@
       <div class="content section">
         <h2 class="title is-1">Dear esteemed friends</h2>
         <p class="is-size-5">You are hereby invited to attend the wedding of two clumsy fools who could not think of anything better to do on their 10th anniversary so decided to just get married and spend money they do not have on a big party.</p>
-        <p>28.02.2020</p>
+        <p class="has-text-weight-semibold">28.02.2020</p>
       </div>
       <figure class="image hero-footer is-5by5">
         <img src="@/assets/happy_couple.png" class="is-inline"/>
