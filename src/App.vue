5756dfb544dcbc7deecf72e72e6236a22fcad088
Cerbeh <cerbeh@gmail.com>
2020-01-18 16:02:10 +0000

    Added animation for header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <section class="hero is-danger is-medium">
+    <section class="hero is-danger is-medium" :class="[ showHome ? 'slidedown' : 'slideup']">
       <div class="hero-body">
         <div class="container">
           <h1 class="title">
@@ -11,10 +11,11 @@
       </div>
     </section>
 
-    <b-tabs animated expanded size="is-large">
+    <b-tabs animated expanded size="is-large" @change="changeView">
 
-      <b-tab-item :label="view.tabName" :key="index" v-for="(view, index) in views">
-        <router-view :name="view.routeName"/>
+      <b-tab-item :key="index" v-for="(view, index) in views">
+        <h5 slot="header" class="is-uppercase">{{view}}</h5>
+        <router-view :name="view"/>
       </b-tab-item>
 
     </b-tabs>
@@ -24,13 +25,24 @@
 export default {
   data() {
     return {
+      activeView: 0,
       views: [
-        {tabName: 'HOME', routeName: 'home'},
-        {tabName:'CEREMONY', routeName: 'ceremony'},
-        {tabName: 'PARTY', routeName: 'party'},
-        {tabName: 'CHARITIES', routeName: 'charities'},
-        {tabName: 'ACCOMODATION', routeName: 'accomodation'}
+        'home',
+        'ceremony',
+        'party',
+        'charities',
+        'accomodation'
       ]
+    }
+  },
+  methods: {
+    changeView(e) {
+      this.activeView = e
+    }
+  },
+  computed: {
+    showHome() {
+      return this.views[this.activeView] !== 'home'
     }
   }
 }
@@ -42,5 +54,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.slideup, .slidedown {
+  max-height: 0;
+  overflow-y: hidden;
+  -webkit-transition: max-height 0.5s ease-in-out;
+  -moz-transition: max-height 0.5s ease-in-out;
+  -o-transition: max-height 0.5s ease-in-out;
+  transition: max-height 0.5s ease-in-out;
+}
+.slidedown {
+  max-height: 50vh ;
 }
 </style>
