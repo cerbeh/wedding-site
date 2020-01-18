d41fdcf3e9e9e316ddc565d8b977e498c4216eea
Cerbeh <cerbeh@gmail.com>
2020-01-18 16:43:32 +0000

    Added second photo and extra class stylings to text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,18 @@
 <template lang="html">
   <div class="hero is-fullheight">
     <div class="hero-body">
+      <figure class="image hero-footer is-5by5">
+        <img src="@/assets/happy_couple2.jpg" class="is-inline"/>
+      </figure>
       <div class="content section">
-        <h2>Dear esteemed friends</h2>
-        <p>You are hereby invited to attend the wedding of two clumsy fools who could not think of anything better to do on their 10th anniversary so decided to just get married and spend money they do not have on a big party.</p>
+        <h2 class="title is-1">Dear esteemed friends</h2>
+        <p class="is-size-5">You are hereby invited to attend the wedding of two clumsy fools who could not think of anything better to do on their 10th anniversary so decided to just get married and spend money they do not have on a big party.</p>
         <p>28.02.2020</p>
       </div>
+      <figure class="image hero-footer is-5by5">
+        <img src="@/assets/happy_couple.png" class="is-inline"/>
+      </figure>
     </div>
-    <figure class="image is-508x508 hero-footer" style="max-height: 50vh">
-      <!-- <img src="http://pubonthepark.com/wp-content/uploads/2017/06/Bears-Den.jpg" alt="image of the bears den in pub on the park" v-if=""> -->
-      <img src="@/assets/happy_couple.png" class="is-inline"/>
-    </figure>
   </div>
 </template>
 
