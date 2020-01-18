bc1be76d5b3755ebc8091dc22c33774e623321a3
Cerbeh <cerbeh@gmail.com>
2020-01-18 16:03:56 +0000

    Removed lorem ipsm and replaced with text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template lang="html">
   <div class="hero is-fullheight">
     <div class="hero-body">
-      <div class="content">
-        <h2>Invitation thing</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <div class="content section">
+        <h2>Dear esteemed friends</h2>
+        <p>You are hereby invited to attend the wedding of two clumsy fools who could not think of anything better to do on their 10th anniversary so decided to just get married and spend money they do not have on a big party.</p>
         <p>28.02.2020</p>
       </div>
     </div>
+    <figure class="image is-508x508 hero-footer" style="max-height: 50vh">
+      <!-- <img src="http://pubonthepark.com/wp-content/uploads/2017/06/Bears-Den.jpg" alt="image of the bears den in pub on the park" v-if=""> -->
+      <img src="@/assets/happy_couple.png" class="is-inline"/>
+    </figure>
   </div>
 </template>
 
