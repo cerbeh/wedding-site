b781abafb1f4cd23948befd18c1908b382596937
Cerbeh <cerbeh@gmail.com>
2020-01-18 16:04:41 +0000

    Added simple paragraph component for site wide use

---
+++ src/components/CardContent.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="content">
+    <Paragraph v-for="(text, index) in paragraphs" :header="text.header" :content="text.content" :key="`paragraph-${index}`"/>
+  </div>
+</template>
+
+<script>
+import Paragraph from '@/components/Paragraph'
+
+export default {
+  name: 'card-content',
+  props: { paragraphs: Array },
+  components: { Paragraph }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/Paragraph.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <p>
+    <span class="has-text-weight-semibold">{{header}}</span>
+    <template v-if="typeof content === 'string'">
+        {{content}}
+    </template>
+    <template v-if="Array.isArray(content)">
+      <div :class="{ 'is-inline': index === 0 }" v-for="(line, index) in content" :key="`paragraph-line-${index}`">
+        {{line}}
+      </div>
+    </template>
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    header: String,
+    content: [ String, Array ]
+  },
+  name: 'paragraph'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
