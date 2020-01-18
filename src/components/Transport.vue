ec1b110bb648397d6b61a3dbd84b84d910f1e350
Cerbeh <cerbeh@gmail.com>
2020-01-18 16:04:21 +0000

    Created transport component due to repetition

---
+++ src/components/Transport.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div>
+    <Paragraph v-for="(text, index) in paragraphs" :header="text.header" :content="text.content" :key="`paragraph-transport-${index}`"/>
+  </div>
+</template>
+
+<script>
+import Paragraph from '@/components/Paragraph'
+export default {
+  data() {
+    return {
+      paragraphs: [
+        { header: 'Buses: ', content: '277, 254, D6, 106, 55, 30, 359, 276, W15'},
+        { header: 'Overground: ', content: 'Hackney Central, London Fields, Cambridge Heath' },
+        { header: 'Underground: ', content: 'Bethnal green, and then a 254, 106 or D6'}
+      ]
+    }
+  },
+  components: { Paragraph }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
