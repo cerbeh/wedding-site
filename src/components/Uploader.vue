4c54ce6229ed17c8e9df949428ab7ba2287029eb
Cerbeh <cerbeh@gmail.com>
2020-03-02 22:00:59 +0000

    Added cloudinary cdn and created uploader

---
+++ src/components/Uploader.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+    <b-button expanded @click="cloudinaryWidget.open()" type="is-info" rounded>Upload Files</b-button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cloudinaryWidget: null,
+    }
+  },
+  mounted() {
+    this.cloudinaryWidget = cloudinary.createUploadWidget({
+      cloudName: 'dhg3jfquj',
+      uploadPreset: 'ml_default',
+    }, (error, result) => {
+      if (!error && result && result.event === "success") {
+        this.$buefy.toast.open({
+          message: 'Image uploaded successfully!',
+          type: 'is-success'
+        })
+      }
+    })
+  },
+}
+</script>
+
+<style lang="css" scoped>
+</style>
