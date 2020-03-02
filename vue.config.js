a894f21acf6e8940498e5b614b4556c41e278188
Cerbeh <cerbeh@gmail.com>
2020-03-02 22:01:16 +0000

    Added config for proxy to BE

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-    publicPath: '',
+  publicPath: '',
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3030',
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
 }
