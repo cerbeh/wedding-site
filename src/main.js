b68b7e6477ba5e10218c667cfa650146750c35fe
Cerbeh <cerbeh@gmail.com>
2020-03-02 22:03:27 +0000

    Added axios to Vue prototype to send request to load pictures

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Buefy from 'buefy';
+import axios from 'axios'
+
+const headers = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+};
+const baseURL = process.env.NODE_ENV === 'development' ? '/api' : 'https://martin.ruudlinssen.com/api';
+
+const $axios = axios.create({
+  headers,
+  baseURL,
+  withCredentials: false,
+});
+
+Vue.prototype.$http = $axios
 
 import 'buefy/dist/buefy.css';
 
